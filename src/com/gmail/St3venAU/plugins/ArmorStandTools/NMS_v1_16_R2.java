d0cc1ebf0e2d5ca964d24973bbea3e4d91e75c98
zombachu <zombachu@gmail.com>
2020-08-21 15:07:43 -1000

    Fix 1.16.2 support

--- src/com/gmail/St3venAU/plugins/ArmorStandTools/NMS_v1_16_R2.java
+++ src/com/gmail/St3venAU/plugins/ArmorStandTools/NMS_v1_16_R2.java
@@ -1,12 +1,12 @@
 package com.gmail.St3venAU.plugins.ArmorStandTools;
 
 @SuppressWarnings("unused")
-class NMS_v1_16_R1 extends NMS {
+class NMS_v1_16_R2 extends NMS {
 
-    public NMS_v1_16_R1(String nmsVersion) {
+    public NMS_v1_16_R2(String nmsVersion) {
         super(
                 nmsVersion,
-                "bA"
+                "bv"
         );
     }
 
